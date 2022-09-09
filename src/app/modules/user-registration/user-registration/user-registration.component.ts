4918daef615667285f687b79846ca2b9f43f952f
Jeans Carlos Rodriguez <jcdeveloper23@gmail.com>
2022-09-08 21:46:10 -0500

    Init integration PayPhone

--- src/app/modules/user-registration/user-registration/user-registration.component.ts
+++ src/app/modules/user-registration/user-registration/user-registration.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { UserService } from 'app/services/user/user.service';
 import swal from 'sweetalert2';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import * as archivePayment from "./../../../../assets/js/function.js";
 
 @Component({
   selector: 'app-user-registration',
@@ -12,10 +13,29 @@ import {v4 as uuidv4} from 'uuid';
 export class UserRegistrationComponent implements OnInit {
 
   public user: User;
-  public validador=true; 
-  seleccionado=false;
-  selected='';
-  constructor(private userService:UserService) { }
+  public validador = true;
+  seleccionado = false;
+  selected = '';
+
+  public roles: Array<Rol> = [
+    {
+      opt: 'Estudiante',
+      value: 5.00,
+    },
+    {
+      opt: 'Profesional externo',
+      value: 10.00,
+    },
+    {
+      opt: 'Profesional asociado',
+      value: 15.00,
+    },
+  ]
+
+  public rolSelected: Rol;
+  public showPpButton: boolean = false;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.user = {
@@ -23,27 +43,27 @@ export class UserRegistrationComponent implements OnInit {
       user_name: '',
       user_lastname: '',
       user_ci: '',
-      user_direction:'',
+      user_direction: '',
       user_email: '',
       user_phone: ''
     };
   }
 
-   /**
-   * *** Funcion para almacenar los mensajes del usuario ***
-   * *** Se obtine la información del formulario de contacto ***
-   * @param user
-   * @param valid
-   */
+  /**
+  * *** Funcion para almacenar los mensajes del usuario ***
+  * *** Se obtine la información del formulario de contacto ***
+  * @param user
+  * @param valid
+  */
   onSaveUser(user: User, valid: boolean) {
     if (valid && this.validador) {
-      user.user_rol=this.selected;
-      if(this.selected=='Estudiante'){
-        user.user_pay=20;
-      }else if(this.selected=='Profecional externo'){
-        user.user_pay=30;
-      }else if(this.selected=='Profecional asociado'){
-        user.user_pay=10;
+      user.user_rol = this.selected;
+      if (this.selected == 'Estudiante') {
+        user.user_pay = 20;
+      } else if (this.selected == 'Profecional externo') {
+        user.user_pay = 30;
+      } else if (this.selected == 'Profecional asociado') {
+        user.user_pay = 10;
       }
       user.user_id = uuidv4();
       this.userService.createUser(user).then(() => {
@@ -53,7 +73,7 @@ export class UserRegistrationComponent implements OnInit {
           user_name: '',
           user_lastname: '',
           user_ci: '',
-          user_direction:'',
+          user_direction: '',
           user_email: '',
           user_phone: '',
           user_rol: '',
@@ -62,56 +82,85 @@ export class UserRegistrationComponent implements OnInit {
       });
     }
   }
-  
+
   validadorDeCedula() {
     let cedula = this.user.user_ci;
     let cedulaCorrecta = false;
-    if (cedula.length == 10)
-    {    
-        let tercerDigito = parseInt(cedula.substring(2, 3));
-        if (tercerDigito < 6) {
-            let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];       
-            let verificador = parseInt(cedula.substring(9, 10));
-            let suma:number = 0;
-            let digito:number = 0;
-            for (let i = 0; i < (cedula.length - 1); i++) {
-                digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];      
-                suma += ((parseInt((digito % 10)+'') + (parseInt((digito / 10)+''))));
-            }
-            suma= Math.round(suma);
-            if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10)== verificador)) {
-                cedulaCorrecta = true;
-            } else if ((10 - (Math.round(suma % 10))) == verificador) {
-                cedulaCorrecta = true;
-            } else {
-                cedulaCorrecta = false;
-            }
-        } else {
-            cedulaCorrecta = false;
+    if (cedula.length == 10) {
+      let tercerDigito = parseInt(cedula.substring(2, 3));
+      if (tercerDigito < 6) {
+        let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];
+        let verificador = parseInt(cedula.substring(9, 10));
+        let suma: number = 0;
+        let digito: number = 0;
+        for (let i = 0; i < (cedula.length - 1); i++) {
+          digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+          suma += ((parseInt((digito % 10) + '') + (parseInt((digito / 10) + ''))));
         }
-    } else {
+        suma = Math.round(suma);
+        if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10) == verificador)) {
+          cedulaCorrecta = true;
+        } else if ((10 - (Math.round(suma % 10))) == verificador) {
+          cedulaCorrecta = true;
+        } else {
+          cedulaCorrecta = false;
+        }
+      } else {
         cedulaCorrecta = false;
+      }
+    } else {
+      cedulaCorrecta = false;
     }
-    
+
     console.log(cedulaCorrecta);
-    if (cedulaCorrecta==true) {
+    if (cedulaCorrecta == true) {
       let ob;
-      let consulta=this.userService.getUserByCI(cedula).subscribe(data=>{ 
-        ob=data;
-        if(ob.length>0){
-          this.validador=false;
-        }else{
-          this.validador=true;
+      let consulta = this.userService.getUserByCI(cedula).subscribe(data => {
+        ob = data;
+        if (ob.length > 0) {
+          this.validador = false;
+        } else {
+          this.validador = true;
         }
       });
-    }else{
-      this.validador=false;
+    } else {
+      this.validador = false;
     }
     // console.log(this.validador);
   }
 
-  validarSeleccion(){
-    this.seleccionado=true;
+  validarSeleccion() {
+    this.showPpButton = false;
+    this.seleccionado = true;
+    console.log('*** validarSeleccion ***');
+    console.log(this.selected);
+
+    this.roles.forEach((rol: Rol) => {
+      if (rol.opt == this.selected) {
+        console.log(rol.opt);
+        console.log(rol.value);
+        this.rolSelected = rol;
+      }
+    });
   }
 
+  public async InitPaymentWhitPayphone(user: User, valid: boolean) {
+    this.showPpButton = false;
+
+    setTimeout(async () => {
+      this.showPpButton = true;
+      setTimeout(async () => {
+        var idd = new Date().getTime();
+        console.log(this.rolSelected.value);
+        var response = await archivePayment.InitPaymentWhitPayphone(
+          (this.rolSelected.value * 100), idd, idd,
+        );
+      }, 200);
+    }, 100);
+  }
+}
+
+class Rol {
+  opt?: string;
+  value?: number;
 }
--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -1,5 +1,71 @@
-$(document).ready(function() {
-    AOS.init({
-      once: true
-    });     
+var firestore;
+$(document).ready(function () {
+  AOS.init({
+    once: true
   });
+
+  console.log('*** Cargando ***');
+  var config = {
+    apiKey: "AIzaSyBPqPck4HvVWkDcjn4aiPiEitqHVNUGNcA",
+    authDomain: "paginaweb-5ab05.firebaseapp.com",
+    projectId: "paginaweb-5ab05",
+    storageBucket: "paginaweb-5ab05.appspot.com",
+    messagingSenderId: "172608828499",
+    appId: "1:172608828499:web:f179995a86cffa23df82a6"
+  };
+
+  firebase.initializeApp(config);
+  firestore = firebase.firestore();
+});
+
+export function InitPaymentWhitPayphone(total, id, order_transaccion_id) {
+  console.log(id.toString());
+  console.log(total);
+  console.log(total.toFixed(0));
+  var totalAmount = total.toFixed(0);
+  console.log(parseInt(totalAmount));
+  payphone.Button({
+      token: 'A1aBrrNGc1THO366NBPHRbBe-EXUDun8yx10teEjue_0pXqtl_sC8IOvXTDDPAzff0zCm4is3vm3NS_ghim_unz8EOjDl87lWSBMEuj1M2HOnyZvqur7BXI-cOxDUAo_1iJhqkOtaPjVqBHevpf_64as-lkRjrEZLum3IQWf10X6fcawC4W3BMwseYooynSroCYpfWZhfi-P3gWF0a1XglTP-3vYPKqkFdfb6OTvrUBGbkT0C1lPBCBYWM-tncRZGGMKo-SIbkzQjtDAJYNulWgUMNzQO3qb04zYR7SGaoaz_lZfrlbXiGqGO4WBU9x6hJv6Yg',
+      btnHorizontal: true,
+      btnCard: true,
+      createOrder: function (actions) {
+          return actions.prepare({
+              amount: parseInt(totalAmount),
+              amountWithoutTax: parseInt(totalAmount),
+              currency: "USD",
+              clientTransactionId: id
+          });
+      },
+      onComplete: function (model, actions) {
+          //Se confirma el pago realizado
+          actions.confirm({
+              id: model.id,
+              clientTxId: model.clientTxId
+          }).then(function (value) {
+              console.log('*** Respuesta de payphone despues de pagar ***');
+              console.log(value);
+              // ***AQUI***
+              // var ref = firestore.collection("orders").doc(order_transaccion_id).collection('payment').doc(id.toString());
+              // ref.set(
+              //     value
+              // ).then(function () {
+              //     if (value.transactionStatus == "Approved") {
+              //         alert('Su orden ha sido generado correctamente!');
+              //         window.location.assign("https://4woman.ec/"),
+              //             localStorage.removeItem("arrayProductCartCache");
+              //         localStorage.removeItem("order");
+              //     } else {
+              //         alert('Se ha presentado un error: ' + value.transactionStatus + ': ' + value.message);
+              //     }
+              // }).catch(function (e) {
+              //     console.log(e);
+              // });
+          }).catch(function (err) {
+              console.log('*** Respuesta ERROR de payphoone despues de pagar ***');
+              console.log(err);
+          });
+      }
+  }).render('#pp-button');
+
+  return '*** Respuesta de payphoone ***'
+}
